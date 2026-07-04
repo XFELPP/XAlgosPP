8ce4b4682658ccf8e4bdd32856154ece79bf4ae1
gadorlhiac <gadorlhiac@gmail.com>
2026-07-04 13:37:03 -0700

    BUG Fix issue with non-inline DType conversion function by adding TU. Rejigger meson deps.

--- src/lib/xalgospp/utilities/interconversion.hh
+++ src/lib/xalgospp/utilities/interconversion.hh
@@ -20,9 +20,9 @@
 #ifndef XALGOSPP_UTILITIES_INTERCONVERSION_HH
 #define XALGOSPP_UTILITIES_INTERCONVERSION_HH
 
-#include "ncarray/dtype.hh"
-
 #include <Eigen/Dense>
+#include <ncarray/dtype.hh>
+#include <ncarray/ncarrays.hh>
 
 #include <stdexcept>
 #include <string>
@@ -50,48 +50,7 @@ namespace xalgospp {
    * @exception invalid_type_string Thrown if the dtype_str is an unrecognized type
    *            representation.
    */
-  ncarray::DType string_to_dtype(const std::string& dtype_str) {
-    if (dtype_str == "bool" || dtype_str == "bool_") {
-      return ncarray::DType::bool_;
-    } else if (dtype_str == "char" || dtype_str == "char_") {
-      return ncarray::DType::char_;
-    } else if (dtype_str == "uint8") {
-      return ncarray::DType::uint8;
-    } else if (dtype_str == "uint16") {
-      return ncarray::DType::uint16;
-    } else if (dtype_str == "uint32") {
-      return ncarray::DType::uint32;
-    } else if (dtype_str == "uint64") {
-      return ncarray::DType::uint64;
-    } else if (dtype_str == "int8") {
-      return ncarray::DType::int8;
-    } else if (dtype_str == "int16") {
-      return ncarray::DType::int16;
-    } else if (dtype_str == "int32") {
-      return ncarray::DType::int32;
-    } else if (dtype_str == "int64") {
-      return ncarray::DType::int64;
-    /* float16/half not currently supported. */
-    } else if (dtype_str == "float32") {
-      return ncarray::DType::float32;
-    } else if (dtype_str == "float64" || dtype_str == "double") {
-      return ncarray::DType::float64;
-    } else if (dtype_str == "float96"  ||
-               dtype_str == "float128" ||
-               dtype_str == "longdouble") {
-      return ncarray::DType::float128;
-    } else if (dtype_str == "complex64" || dtype_str == "csingle") {
-      return ncarray::DType::complex64;
-    } else if (dtype_str == "complex128" || dtype_str == "cdouble") {
-      return ncarray::DType::complex128;
-    } else if (dtype_str == "complex192" ||
-               dtype_str == "complex256" ||
-               dtype_str == "clongdouble") {
-      return ncarray::DType::complex256;
-    }
-
-    throw invalid_type_string("Unsupported type str repr: " + dtype_str + "!");
-  }
+  ncarray::DType string_to_dtype(const std::string& dtype_str);
 
   /**
    * Convert an NCArrayView to an Eigen Map.
